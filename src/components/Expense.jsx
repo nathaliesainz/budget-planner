55f57713072b08fc58abbaa99219b0bbee1b4b79
nathaliesainz <nathaliesainz13@gmail.com>
2023-03-20 11:31:39 -0600

    Add swipe eventsto expenses cards

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,45 +1,73 @@
-import React from "react"
+import React from "react";
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 import { formattedDate } from "../helpers";
 
-import SavingsIcon from '../img/icono_ahorro.svg'
-import HouseIcon from '../img/icono_casa.svg'
-import FoodIcon from '../img/icono_comida.svg'
-import OthersIcon from '../img/icono_gastos.svg'
-import HobbiesIcon from '../img/icono_ocio.svg'
-import HealthIcon from '../img/icono_salud.svg'
-import SubscriptionsIcon from '../img/icono_suscripciones.svg'
+import SavingsIcon from "../img/icono_ahorro.svg";
+import HouseIcon from "../img/icono_casa.svg";
+import FoodIcon from "../img/icono_comida.svg";
+import OthersIcon from "../img/icono_gastos.svg";
+import HobbiesIcon from "../img/icono_ocio.svg";
+import HealthIcon from "../img/icono_salud.svg";
+import SubscriptionsIcon from "../img/icono_suscripciones.svg";
 
 const iconsDictionary = {
-  savings : SavingsIcon,
-  house : HouseIcon,
-  food : FoodIcon,
-  others : OthersIcon,
-  hobbies : HobbiesIcon,
-  health : HealthIcon,
-  subscriptions : SubscriptionsIcon
-}
+  savings: SavingsIcon,
+  house: HouseIcon,
+  food: FoodIcon,
+  others: OthersIcon,
+  hobbies: HobbiesIcon,
+  health: HealthIcon,
+  subscriptions: SubscriptionsIcon,
+};
 
-const Expense = ({expense}) => {
-  const {category, name, amount, id, date} = expense;
-  return (
-    <div className="gasto sombra">
-        <div className="contenido-gasto">
-          <img 
-            src={iconsDictionary[category]} 
-            alt="Expense Icon" 
-          />
-          <div className="descripcion-gasto">
-            <p className="categoria">{category}</p>
-            <p className="nombre-gasto">{name}</p>
-            <p className="fecha-gasto">
-                Added on: {''}
-                <label>{formattedDate(date)}</label>
-            </p>
-          </div>
-        </div>
+const Expense = ({ expense }) => {
+  const { category, name, amount, id, date } = expense;
 
-        <p className="cantidad-gasto">${amount}</p>
-    </div>
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => console.log('Editing...')}>
+        Edit
+      </SwipeAction>
+    </LeadingActions>
   )
-}
-export default Expense
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={() => console.log('Deleting...')}>
+        Delete
+      </SwipeAction>
+    </TrailingActions>
+  )
+
+  return (
+    <SwipeableList>
+        <SwipeableListItem
+            leadingActions={leadingActions()}
+            trailingActions={trailingActions()}
+        >
+            <div className="gasto sombra">
+              <div className="contenido-gasto">
+                <img src={iconsDictionary[category]} alt="Expense Icon" />
+                <div className="descripcion-gasto">
+                  <p className="categoria">{category}</p>
+                  <p className="nombre-gasto">{name}</p>
+                  <p className="fecha-gasto">
+                    Added on: {""}
+                    <label>{formattedDate(date)}</label>
+                  </p>
+                </div>
+              </div>
+              <p className="cantidad-gasto">${amount}</p>
+            </div>
+        </SwipeableListItem>
+    </SwipeableList>
+  );
+};
+export default Expense;
